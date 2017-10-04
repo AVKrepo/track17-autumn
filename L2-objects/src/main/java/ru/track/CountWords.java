b58148930bcd151c97d968c14a68315cb36221d5
AVKdeveloper <kosov-av@ya.ru>
2017-10-04 16:40:36 +0300

    Done task for practice on classes

--- L2-objects/src/main/java/ru/track/CountWords.java
+++ L2-objects/src/main/java/ru/track/CountWords.java
@@ -1,6 +1,8 @@
 package ru.track;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 
 /**
@@ -40,19 +42,43 @@ public class CountWords {
      * @return - целое число - сумма всех чисел из файла
      */
     public long countNumbers(File file) throws Exception {
-        return 0;
+        long result = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            try {
+                result += Integer.parseInt(line);
+            } catch(NumberFormatException exception) {
+                // ignore
+            }
+        }
+        return result;
     }
 
 
     /**
      * Метод на вход принимает объект File, изначально результат= ""
-     * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
+     * Нужно пройти по всем строкам файла, и если в строка не пустая, не число и не является skipWord
      * то надо присоединить ее к результату через пробел
      * @param file - файл с данными
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        return null;
+        StringBuilder result = new StringBuilder("");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("") || line.equals(skipWord)) {
+                continue;
+            }
+            try {
+                Integer.parseInt(line);
+            } catch(NumberFormatException exception) {
+                // not a number, it's our case
+                result.append(line + " ");
+            }
+        }
+        return result.toString();
     }
 
 }
